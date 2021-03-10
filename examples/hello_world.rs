bc71d8ebe39a4d50e897e0800389477899302045
Carl Lerche <me@carllerche.com>
2021-03-09 22:25:39 -0800

    nested

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -5,29 +5,56 @@ use core::any::TypeId;
 
 struct HelloWorld {
     hello: &'static str,
-    world: usize,
+    world: World,
 }
 
-static FIELDS: &'static [FieldDefinition] = &[
+struct World {
+    answer: usize,
+}
+
+static HELLO_WORLD_FIELDS: &'static [FieldDefinition] = &[
     FieldDefinition { name: "hello", ty: Type::String },
-    FieldDefinition { name: "world", ty: Type::Usize },
+    FieldDefinition { name: "world", ty: Type::Valuable },
 ];
 
 impl Valuable for HelloWorld {
     fn fields(&self) -> Fields {
         Fields {
             type_id: TypeId::of::<Self>(),
-            definitions: FIELDS,
+            definitions: HELLO_WORLD_FIELDS,
         }
     }
 
     fn get(&self, field: &Field) -> Option<Value<'_>> {
         if field.type_id != TypeId::of::<Self>() {
             None
-        } else if field.definition == &FIELDS[0] {
+        } else if field.definition == &HELLO_WORLD_FIELDS[0] {
             Some(Value::String(&self.hello))
-        } else if field.definition == &FIELDS[1] {
-            Some(Value::Usize(self.world))
+        } else if field.definition == &HELLO_WORLD_FIELDS[1] {
+            Some(Value::Valuable(&self.world))  
+        } else {
+            None
+        }
+    }
+}
+
+static WORLD_FIELDS: &'static [FieldDefinition] = &[
+    FieldDefinition { name: "answer", ty: Type::Usize },
+];
+
+impl Valuable for World {
+    fn fields(&self) -> Fields {
+        Fields {
+            type_id: TypeId::of::<Self>(),
+            definitions: WORLD_FIELDS,
+        }
+    }
+
+    fn get(&self, field: &Field) -> Option<Value<'_>> {
+        if field.type_id != TypeId::of::<Self>() {
+            None
+        } else if field.definition == &WORLD_FIELDS[0] {
+            Some(Value::Usize(self.answer))
         } else {
             None
         }
@@ -37,7 +64,9 @@ impl Valuable for HelloWorld {
 fn main() {
     let hello_world = HelloWorld {
         hello: "wut",
-        world: 42,
+        world: World {
+            answer: 42,
+        },
     };
 
     let value = Value::Valuable(&hello_world);
