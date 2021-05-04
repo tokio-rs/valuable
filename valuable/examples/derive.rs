a6b57641f87e0a89bac8f17df61b73895cce8c95
Carl Lerche <me@carllerche.com>
2021-05-03 18:02:06 -0700

    add derive hashmap

--- valuable/examples/derive.rs
+++ valuable/examples/derive.rs
@@ -1,5 +1,7 @@
 use valuable::Valuable;
 
+use std::collections::HashMap;
+
 // `Debug` not implemented for struct, the debug implementation is going via
 // valuable.
 #[derive(Valuable)]
@@ -7,14 +9,18 @@ struct Person {
     name: String,
     age: u8,
     phones: Vec<String>,
+    favorites: HashMap<String, String>,
 }
 
 fn main() {
-    let p = Person {
+    let mut p = Person {
         name: "John Doe".to_string(),
         age: 42,
         phones: vec!["876-5309".to_string()],
+        favorites: HashMap::new(),
     };
+
+    p.favorites.insert("color".to_string(), "blue".to_string());
 
     println!("{:#?}", p.as_value());
 }
