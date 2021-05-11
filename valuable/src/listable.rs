3622078d1650b7080a176aa8fd4f250ecae995fb
Taiki Endo <te316e89@gmail.com>
2021-05-12 08:21:04 +0900

    impl Listable for std collections (#13)

--- valuable/src/listable.rs
+++ valuable/src/listable.rs
@@ -34,30 +34,90 @@ impl<L: ?Sized + Listable> Listable for alloc::sync::Arc<L> {
     }
 }
 
-impl<T: Valuable> Listable for &'_ [T] {
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
-    }
+macro_rules! slice {
+    (
+        $(
+            $(#[$attrs:meta])*
+            ($($generics:tt)*) $ty:ty,
+        )*
+    ) => {
+        $(
+            $(#[$attrs])*
+            impl<$($generics)*> Valuable for $ty {
+                fn as_value(&self) -> Value<'_> {
+                    Value::Listable(self as &dyn Listable)
+                }
+
+                fn visit(&self, visit: &mut dyn Visit) {
+                    T::visit_slice(self, visit);
+                }
+            }
+
+            $(#[$attrs])*
+            impl<$($generics)*> Listable for $ty {
+                fn size_hint(&self) -> (usize, Option<usize>) {
+                    (self.len(), Some(self.len()))
+                }
+            }
+        )*
+    };
 }
 
-#[cfg(feature = "alloc")]
-impl<T: Valuable> Listable for alloc::boxed::Box<[T]> {
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
-    }
+slice! {
+    (T: Valuable) &'_ [T],
+    #[cfg(feature = "alloc")]
+    (T: Valuable) alloc::boxed::Box<[T]>,
+    #[cfg(feature = "alloc")]
+    (T: Valuable) alloc::rc::Rc<[T]>,
+    #[cfg(feature = "alloc")]
+    (T: Valuable) alloc::sync::Arc<[T]>,
+    (T: Valuable, const N: usize) [T; N],
+    #[cfg(feature = "alloc")]
+    (T: Valuable) alloc::vec::Vec<T>,
 }
 
-impl<T: Valuable, const N: usize> Listable for [T; N] {
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
-    }
+macro_rules! collection {
+    (
+        $(
+            $(#[$attrs:meta])*
+            ($($generics:tt)*) $ty:ty,
+        )*
+    ) => {
+        $(
+            $(#[$attrs])*
+            impl<$($generics)*> Valuable for $ty {
+                fn as_value(&self) -> Value<'_> {
+                    Value::Listable(self as &dyn Listable)
+                }
+
+                fn visit(&self, visit: &mut dyn Visit) {
+                    for value in self.iter() {
+                        visit.visit_value(value.as_value());
+                    }
+                }
+            }
+
+            $(#[$attrs])*
+            impl<$($generics)*> Listable for $ty {
+                fn size_hint(&self) -> (usize, Option<usize>) {
+                    (self.len(), Some(self.len()))
+                }
+            }
+        )*
+    };
 }
 
-#[cfg(feature = "alloc")]
-impl<T: Valuable> Listable for alloc::vec::Vec<T> {
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
-    }
+collection! {
+    #[cfg(feature = "alloc")]
+    (T: Valuable) alloc::collections::VecDeque<T>,
+    #[cfg(feature = "alloc")]
+    (T: Valuable) alloc::collections::LinkedList<T>,
+    #[cfg(feature = "alloc")]
+    (T: Valuable + Ord) alloc::collections::BinaryHeap<T>,
+    #[cfg(feature = "alloc")]
+    (T: Valuable + Ord) alloc::collections::BTreeSet<T> ,
+    #[cfg(feature = "std")]
+    (T: Valuable + Eq + std::hash::Hash, H: std::hash::BuildHasher) std::collections::HashSet<T, H>,
 }
 
 impl fmt::Debug for dyn Listable + '_ {
