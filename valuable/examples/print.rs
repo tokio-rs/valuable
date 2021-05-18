0926e546e670703be317a292f419f880234ae8b8
Taiki Endo <te316e89@gmail.com>
2021-05-18 19:51:14 +0900

    Rename NamedValues::entries to iter and impl IntoIterator for NamedValues

--- valuable/examples/print.rs
+++ valuable/examples/print.rs
@@ -50,7 +50,7 @@ impl Visit for Print {
     }
 
     fn visit_named_fields(&mut self, named_values: &NamedValues<'_>) {
-        for (field, value) in named_values.entries() {
+        for (field, value) in named_values {
             print!("{}- {}: ", self.0, field.name());
             value.visit(self);
         }
