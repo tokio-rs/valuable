93fbaa4d8d6f1373975fdb3191cb88290f928ee9
Carl Lerche <me@carllerche.com>
2021-05-22 11:47:00 -0700

    document library root and Valuable macro (#46)

--- valuable-derive/src/lib.rs
+++ valuable-derive/src/lib.rs
@@ -5,6 +5,24 @@ mod expand;
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
+/// Derive a `Valuable` implementation for a struct or enum.
+///
+/// # Examples
+///
+/// ```
+/// use valuable::Valuable;
+///
+/// #[derive(Valuable)]
+/// struct HelloWorld {
+///     message: Message,
+/// }
+///
+/// #[derive(Valuable)]
+/// enum Message {
+///     HelloWorld,
+///     Custom(String),
+/// }
+/// ```
 #[proc_macro_derive(Valuable, attributes(valuable))]
 pub fn derive_valuable(input: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(input as syn::DeriveInput);
