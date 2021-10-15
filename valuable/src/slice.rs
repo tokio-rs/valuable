06fdc401bd6a2716e7712c3c1d8bf8abd0863c69
Eliza Weisman <eliza@buoyant.io>
2021-10-15 12:28:16 -0700

    Add `Slice::is_empty()` method
    Clippy lints about types which have a public `len` method but no
    corresponding `is_empty` method. Therefore, I've added an `is_empty`
    method that returns `true` if `self.len() > 0`.
    
    Since this is a new API that we'll have to support, it would also be
    fine to solve this by _not_ adding the method and just suppressing the
    warning...but it seems worth having for consistency with the underlying
    slice's methods? And, I don't see how this could really pose a
    forward-compatibility hazard, since `Slice` is...always going to be
    backed by an actual slice...

--- valuable/src/slice.rs
+++ valuable/src/slice.rs
@@ -75,6 +75,37 @@ macro_rules! slice {
                 }
             }
 
+
+            /// Returns `true` if the slice is not empty.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use valuable::Slice;
+            ///
+            /// let slice = Slice::U32(&[1, 1, 2, 3, 5]);
+            /// assert!(!slice.is_empty());
+            /// ```
+            /// ```
+            /// # use valuable::Slice;
+            /// let slice = Slice::U32(&[]);
+            /// assert!(slice.is_empty());
+            /// ```
+            pub fn is_empty(&self) -> bool {
+                self.len() == 0
+            }
+
+            /// Returns the number of elements in the slice
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use valuable::Slice;
+            ///
+            /// let slice = Slice::U32(&[1, 1, 2, 3, 5]);
+            /// assert_eq!(5, slice.len());
+            /// ```
+
             /// Returns an iterator over the slice.
             ///
             /// # Examples
