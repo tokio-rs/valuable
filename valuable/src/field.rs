062794808756195dfaa750abfe2eb82d5c53e331
Bryan Garza <1396101+bryangarza@users.noreply.github.com>
2022-08-17 14:28:19 -0700

    Implement Eq for NamedField

--- valuable/src/field.rs
+++ valuable/src/field.rs
@@ -11,7 +11,7 @@ pub enum Fields<'a> {
 }
 
 /// A named field
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct NamedField<'a>(&'a str);
 
 impl Fields<'_> {
