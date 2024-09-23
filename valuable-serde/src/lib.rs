eca6a6e1c31d914d541786c859c8ed6c81374173
Kevin Guthrie <kevinbartlett@cloudflare.com>
2024-09-22 21:13:33 -0400

    Fixing unused variable in no-std serialization

--- valuable-serde/src/lib.rs
+++ valuable-serde/src/lib.rs
@@ -265,16 +265,12 @@ where
             Value::Path(p) => Serialize::serialize(p, serializer),
             #[cfg(feature = "std")]
             Value::Error(e) => SerializeError(e).serialize(serializer),
-            Value::Renderable(r) => {
-                #[cfg(feature = "alloc")]
-                {
-                    serializer.serialize_str(&r.render_to_string())
-                }
-                #[cfg(not(feature = "alloc"))]
-                {
-                    // Can't serialize a renderable without allocating
-                    serializer.serialize_none()
-                }
+            #[cfg(feature = "alloc")]
+            Value::Renderable(r) => serializer.serialize_str(&r.render_to_string()),
+            #[cfg(not(feature = "alloc"))]
+            Value::Renderable(_) => {
+                // Can't serialize a renderable without allocating
+                serializer.serialize_none()
             }
 
             v => unimplemented!("{:?}", v),
